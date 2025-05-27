ff6ae884073d45c2f599b9dae605d2e64dd25eb8
Trompert-Analysis <benno_trompert@msn.com>
2025-05-27 22:50:56 +0200

    Add files via upload

---
+++ frontend/src/pages/Analyse.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Analyse() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2">Analyse</h1>
+      <p>Inhoud voor de Analyse pagina komt hier.</p>
+    </div>
+  );
+}
+
+export default Analyse;
