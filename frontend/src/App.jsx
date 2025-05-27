921dfa1b746060ce1aa898a9702392794e9ac43b
Trompert-Analysis <benno_trompert@msn.com>
2025-05-27 20:49:58 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Speler from "./pages/Speler";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/speler" element={<Speler />} />
-      </Routes>
-    </Router>
-  );
+    <div className="p-6 text-center text-xl text-gray-800">
+      <h1 className="text-2xl font-bold mb-4">Voetbal Analysetool</h1>
+      <p>Formulier volgt...</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
