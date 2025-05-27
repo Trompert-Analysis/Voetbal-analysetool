3bb06b7c65564ced2fe2d0d0cd4e524b66be5a01
Trompert-Analysis <benno_trompert@msn.com>
2025-05-27 11:52:41 +0200

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+
+app = FastAPI()
+
+class InputData(BaseModel):
+    speler_id: int
+    beoordeling: dict
+    zelfbeoordeling: dict
+
+@app.post("/matchscore")
+def bereken_matchscore(data: InputData):
+    # Dummy matchscore
+    return {
+        "advies": "Je past het best op de positie CAM – 10",
+        "matchscore": 74.2
+    }
