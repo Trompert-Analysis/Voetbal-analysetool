b72a8c6298e8cae375dc019eaf32f48c3ac2ef21
Trompert-Analysis <benno_trompert@msn.com>
2025-05-27 17:01:50 +0200

    Add files via upload

--- frontend/src/pages/Analyse.jsx
+++ frontend/src/pages/Analyse.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Analyse() {
   return (
     <div className="p-4">
--- frontend/src/pages/Speler.jsx
+++ frontend/src/pages/Speler.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Speler() {
   return (
     <div className="p-4">
