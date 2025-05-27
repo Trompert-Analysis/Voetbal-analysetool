e6fb50aeced892e4548de7323d1b4874b17efab1
Trompert-Analysis <benno_trompert@msn.com>
2025-05-27 23:03:44 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Speler from './pages/Speler';
+import My_score from './pages/My_score';
 import Analyse from './pages/Analyse';
 import Team from './pages/Team';
 
