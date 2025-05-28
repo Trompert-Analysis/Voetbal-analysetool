6778d9a510e5085e30f8a4e428aa083e85389911
Trompert-Analysis <benno_trompert@msn.com>
2025-05-28 21:56:42 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Speler from './pages/Speler';
-import Analyse from './pages/Analyse';
-import Team from './pages/Team';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Speler" element={<Speler />} />
-        <Route path="/analyse" element={<Analyse />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+<Route
+  path="/Speler"
+  element={
+    <ProtectedRoute>
+      <Speler />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/analyse"
+  element={
+    <ProtectedRoute>
+      <Analyse />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/team"
+  element={
+    <ProtectedRoute>
+      <Team />
+    </ProtectedRoute>
+  }
+/>
+<Route
+  path="/Dashboard"
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
