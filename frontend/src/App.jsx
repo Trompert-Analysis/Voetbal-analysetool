652bd95156b1ae91dc12c84c9a19deb29e19a461
Trompert-Analysis <benno_trompert@msn.com>
2025-05-28 22:08:28 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,58 @@
-import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from './components/ProtectedRoute';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import Speler from './pages/Speler';
 import Analyse from './pages/Analyse';
 import Team from './pages/Team';
+import Register from './pages/Register';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/Speler"
-        element={
-          <ProtectedRoute>
-            <Speler />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/analyse"
-        element={
-          <ProtectedRoute>
-            <Analyse />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/team"
-        element={
-          <ProtectedRoute>
-            <Team />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/Dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+        
+        <Route
+          path="/Speler"
+          element={
+            <ProtectedRoute>
+              <Speler />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/analyse"
+          element={
+            <ProtectedRoute>
+              <Analyse />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/team"
+          element={
+            <ProtectedRoute>
+              <Team />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/Dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
