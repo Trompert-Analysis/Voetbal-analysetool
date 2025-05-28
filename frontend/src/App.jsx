194fd6c262d39fb6e572acba2a4d89156a2eb2b2
Trompert-Analysis <benno_trompert@msn.com>
2025-05-28 22:04:12 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,47 @@
+import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
+import Speler from './pages/Speler';
+import Analyse from './pages/Analyse';
+import Team from './pages/Team';
+import Dashboard from './pages/Dashboard';
 
-<Route
-  path="/Speler"
-  element={
-    <ProtectedRoute>
-      <Speler />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/analyse"
-  element={
-    <ProtectedRoute>
-      <Analyse />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/team"
-  element={
-    <ProtectedRoute>
-      <Team />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/Dashboard"
-  element={
-    <ProtectedRoute>
-      <Dashboard />
-    </ProtectedRoute>
-  }
-/>
+function App() {
+  return (
+    <Routes>
+      <Route
+        path="/Speler"
+        element={
+          <ProtectedRoute>
+            <Speler />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/analyse"
+        element={
+          <ProtectedRoute>
+            <Analyse />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/team"
+        element={
+          <ProtectedRoute>
+            <Team />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/Dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
