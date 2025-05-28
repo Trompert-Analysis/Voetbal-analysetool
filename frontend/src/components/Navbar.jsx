1179cf0d14cc4436966518ad379890a538c1f809
Trompert-Analysis <benno_trompert@msn.com>
2025-05-28 21:36:34 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <nav className="bg-gray-800 text-white p-4 flex gap-4">
       <Link to="/">Home</Link>
+      <Link to="/Dashboard">Dashboard</Link>
       <Link to="/speler">Speler</Link>
       <Link to="/analyse">Analyse</Link>
       <Link to="/team">Team</Link>
